9b332e5ef3aa765b8e39f88b44e7015fdfc56957
dongasai <1514582970@qq.com>
2023-09-01 00:47:36 +0800

    时间计算

--- dht_client_task/config.php
+++ dht_client_task/config.php
@@ -1,7 +1,7 @@
 <?php
 $rre =  array(
     'daemonize'       => false,//是否后台守护进程
-    'worker_num'      => 2,// 主进程数, 一般为CPU的1至4倍 同时执行任务数量
+    'worker_num'      => Env::get('WORKER_NUM', 1),// 主进程数, 一般为CPU的1至4倍 同时执行任务数量
     'task_worker_num' => Env::get('TASK_WORKER_NUMBER', 10),//task进程的数量 值越大 CPU占用越高
     'server_ip'       => gethostbyname(Env::get('SERVER_IP', '127.0.0.1')) ,//服务端ip
     'server_port'     => Env::get('SERVER_PORT', 31738),//服务端端口
